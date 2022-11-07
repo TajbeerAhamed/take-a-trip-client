423f07082a9e20795cc43e2b33e6f479f4891e16
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-11-08 01:59:39 +0600

    carousel-added

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -2,36 +2,28 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img src="https://placeimg.com/800/200/arch" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle">❮</a> 
-            <a href="#slide2" className="btn btn-circle">❯</a>
-          </div>
-        </div> 
-        <div id="slide2" className="carousel-item relative w-full">
-          <img src="https://placeimg.com/800/200/arch" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle">❮</a> 
-            <a href="#slide3" className="btn btn-circle">❯</a>
-          </div>
-        </div> 
-        <div id="slide3" className="carousel-item relative w-full">
-          <img src="https://placeimg.com/800/200/arch" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle">❮</a> 
-            <a href="#slide4" className="btn btn-circle">❯</a>
-          </div>
-        </div> 
-        <div id="slide4" className="carousel-item relative w-full">
-          <img src="https://placeimg.com/800/200/arch" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" className="btn btn-circle">❮</a> 
-            <a href="#slide1" className="btn btn-circle">❯</a>
-          </div>
-        </div>
-      </div>
+       <div>
+        <div className="carousel mx-auto rounded-xl mt-10 mb-10 w-3/5">
+  <div id="item1" className="carousel-item w-full">
+    <img src="https://images.unsplash.com/photo-1619177383949-f03975e50b19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y294c2JhemFyfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60" className="w-full" />
+  </div> 
+  <div id="item2" className="carousel-item w-full">
+    <img src="https://images.unsplash.com/photo-1642913954997-4aec8d323f64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2FpbnRtYXJ0aW58ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" className="w-full" />
+  </div> 
+  <div id="item3" className="carousel-item w-full">
+    <img src="https://images.unsplash.com/photo-1629739868151-d29aab8b4dd1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c2FqZWt8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" className="w-full" />
+  </div> 
+  <div id="item4" className="carousel-item w-full">
+    <img src="https://images.unsplash.com/photo-1632579219536-24216cfad7f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGJhbmRhcmJhbnxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60" className="w-full" />
+  </div>
+</div> 
+<div className="flex justify-center w-full py-2 gap-2">
+  <a href="#item1" className="btn btn-xs">1</a> 
+  <a href="#item2" className="btn btn-xs">2</a> 
+  <a href="#item3" className="btn btn-xs">3</a> 
+  <a href="#item4" className="btn btn-xs">4</a>
+</div>
+       </div>
     );
 };
 
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -8,7 +8,7 @@ const Blog = () => {
             <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Question And Answer</h2>
             <div className="grid gap-10 md:gap-8 sm:p-3 md:grid-cols-2 lg:px-12 xl:px-32">
                 <div>
-                    <h3 className="font-semibold">Difference between SQL and NoSQL?</h3>
+                    <h3 className="font-semibold text-blue-600 text-2xl">Difference between SQL and NoSQL?</h3>
                     <p ><p>SQL databases are relational,</p> NoSQL databases are non-relational.</p>
                     <p>SQL databases use structured query language and have a predefined schema. </p>
                     <p>NoSQL databases have dynamic schemas for unstructured data.</p>
@@ -17,21 +17,21 @@ const Blog = () => {
                     <p>SQL databases are better for multi-row transactions, while NoSQL is better for unstructured data like documents or JSON.</p>
                 </div>
                 <div>
-                    <h3 className="font-semibold">What is JWT, and how does it work?</h3>
+                    <h3 className="font-semibold text-blue-600 text-2xl">What is JWT, and how does it work?</h3>
                     <p>JWT is JSON Web Token is an open standard that defines a compact and self-contained way for securely transmitting information between parties as a JSON object. This information can be verified and trusted because it is digitally signed. JWTs can be signed using a secret or a public/private key pair using RSA or ECDSA.Basically the identity provider(IdP) generates a JWT certifying user identity and Resource server decodes and verifies the authenticity of the token using secret salt / public key.User sign-in using username and password or google/facebook.
 Authentication server verifies the credentials and issues a jwt signed using either a secret salt or a private key.
 User's Client uses the JWT to access protected resources by passing the JWT in HTTP Authorization header.
 Resource server then verifies the authenticity of the token using the secret salt/ public key.</p>
                 </div>
                 <div>
-                    <h3 className="font-semibold">What is the difference between javascript and NodeJS?</h3>
+                    <h3 className="font-semibold text-blue-600 text-2xl">What is the difference between javascript and NodeJS?</h3>
                     <p> JavaScript is a client-side scripting language that is lightweight, cross-platform, and interpreted. Both Java and HTML include it. Node.js, on the other hand, is a V8-based server-side programming language.
 
 As a result, it is used to create network-centric applications. It's a networked system made for data-intensive real-time applications. If we compare node js vs. python, it is clear that node js will always be the preferred option. JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node.js, on the other hand, is an interpreter or execution environment for the JavaScript programming language. It requires libraries that can be conveniently accessed from JavaScript programming to be more helpful.Any engine may run JavaScript. As a result, writing JavaScript is incredibly easy, and any working environment is similar to a complete browser. Node.js, on the other hand, only enables the V8 engine. Written JavaScript code, on the other hand, can run in any context, regardless of whether the V8 engine is supported.</p>
                 </div>
                 <div>
-                    <h3 className="font-semibold">How does NodeJS handle multiple requests at the same time?</h3>
-                    <p >NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes themGiven a NodeJS application, since Node is single threaded, say if processing involves a Promise.all that takes 8 seconds, does this mean that the client request that comes after this request would need to wait for eight seconds?
+                    <h3 className="font-semibold text-blue-600 text-2x">How does NodeJS handle multiple requests at the same time?</h3>
+                    <p className='text-bold'>NodeJS receives multiple client requests and places them into EventQueue. NodeJS is built with the concept of event-driven architecture. NodeJS has its own EventLoop which is an infinite loop that receives requests and processes themGiven a NodeJS application, since Node is single threaded, say if processing involves a Promise.all that takes 8 seconds, does this mean that the client request that comes after this request would need to wait for eight seconds?
 No. NodeJS event loop is single threaded. The entire server architecture for NodeJS is not single threaded.
 
 Before getting into the Node server architecture, to take a look at typical multithreaded request response model, the web server would have multiple threads and when concurrent requests get to the webserver, the webserver picks threadOne from the threadPool and threadOne processes requestOne and responds to clientOne and when the second request comes in, the web server picks up the second thread from the threadPool and picks up requestTwo and processes it and responds to clientTwo. </p>
