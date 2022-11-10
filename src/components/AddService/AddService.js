e39e3f9f85858b5c7fc2b239f5c74dbc7f26a1a7
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-11-10 23:17:38 +0600

    last commit

--- src/components/AddService/AddService.js
+++ src/components/AddService/AddService.js
@@ -1,19 +1,92 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const AddService = () => {
+	const [form, setForm] = useState({});
+    const [reviews ,setReviews]= useState([]);
+    const { user } = useContext(AuthContext);
+    // console.log(user); 
+
+    // const getReviewData = async()=>{
+    //   const response = await  fetch('https://citro-golpo.vercel.app/reviews')
+    //     const data = await response.json();
+    //     return data;
+    // }
+
+
+    useEffect(()=>{
+        fetch('https://take-a-trip-server.vercel.app/services')
+        .then(res=>res.json())
+        .then(data=>setReviews(data))
+    },[])
+
+    // const reviews = useLoaderData();
+    // console.log(reviews);
+
+    const handleReviewSubmit = event => {
+        event.preventDefault();
+        console.log(form);
+
+
+
+        if (user?.uid) {
+            fetch("https://take-a-trip-server.vercel.app/services", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify(form),
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                    if (data.acknowledged) {
+                        alert("Review Added Successfully");
+                        form.reset();
+                    }
+                })
+                .catch((err) => console.log(err));
+        } else {
+            alert("Please login first to provide review!");
+
+
+
+        }
+    }
+    const handleInputBlur = event => {
+        const value = event.target.value;
+        const field = event.target.name;
+        const newForm = { ...form }
+        newForm[field] = value;
+        setForm(newForm)
+    }
     return (
-        <div>
-            <div className="max-w-xs rounded-md shadow-md bg-gray-900 text-gray-100">
-	<img src="https://source.unsplash.com/random/300x300/?2" alt="" className="object-cover object-center w-full rounded-t-md h-72 bg-gray-500" />
-	<div className="flex flex-col justify-between p-6 space-y-8">
-		<div className="space-y-2">
-			<h2 className="text-3xl font-semibold tracking-wide">Donec lectus leo</h2>
-			<p className="text-gray-100">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
+<div>
+
+<div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-900 text-gray-100">
+	
+	<form onSubmit={handleReviewSubmit} novalidate="" action="" className="space-y-12 ng-untouched ng-pristine ng-valid">
+		<div className="space-y-4">
+			<div>
+				<label for="email" className="block mb-2 text-sm">Add Service</label>
+				<input onChange={handleInputBlur} type="text" name="text" id="text" placeholder="Enter Services" className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100" />
+			</div>
+			<div className="flex flex-col w-full">
+                            <textarea onChange={handleInputBlur} rows="3" type="description" id="description" name="description" placeholder="Message..." className="p-4 rounded-md resize-none dark:text-gray-100 dark:bg-gray-900"></textarea>
+                            <button type="submit" className="py-4 my-8 font-semibold rounded-md dark:text-gray-900 bg-white">Leave Review</button>
+                        </div>
+			<div>
+				
+			
+			</div>
 		</div>
-		<button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-violet-400 text-gray-900">Read more</button>
-	</div>
+		<div className="space-y-2">
+		
+		
+		</div>
+	</form>
 </div>
-        </div>
+</div>
     );
 };
 
--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+
+const PrivateRoute = ({children}) => {
+    const {user,loading} = useContext(AuthContext);
+    const location = useLocation();
+
+    if(loading){
+        return 
+    }
+    if(!user){
+        return <Navigate to ="/login"
+         state={{from: location}}
+         replace></Navigate>
+    }
+    return children;
 };
 
 export default PrivateRoute;
